05cd95eabfddf2d1cac71002763063c6b7f2300c
abhiyadav2345 <abhiyadav2345@gmail.com>
2022-09-25 09:45:29 +0530

    responsive feature added

---
+++ index.js
@@ -0,0 +1,62 @@
+// ============SIDE BAR=======================
+
+const menuItems = document.querySelectorAll(".menu-item");
+
+//============================MESSAGES======================
+
+const messagesNotification = document.querySelector("#messages-notifications");
+
+const messages = document.querySelector(".messages");
+const message = document.querySelectorAll(".message");
+const messageSearch = document.querySelector("#message-search");
+
+//=======================Remove active class for all menu items===============
+const changeActiveItem = () => {
+  menuItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+
+    if (item.id != "notifications") {
+      document.querySelector(".notifications-popup").style.display = "none";
+    } else {
+      document.querySelector(".notifications-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
+
+//=========================MESSAGES==============================
+
+// =============================Search user=================================
+
+const searchMessage = () => {
+  const val = messageSearch.value.toLowerCase();
+  message.forEach((user) => {
+    let name = user.querySelector("h5").textContent.toLowerCase();
+    if (name.indexOf(val) != -1) {
+      user.style.display = "flex";
+    } else {
+      user.style.display = "none";
+    }
+  });
+};
+
+messageSearch.addEventListener("keyup", searchMessage);
+
+//=============================highlight messages card when messages menu item is clicked============================
+messagesNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  messagesNotification.querySelector(".notification-count").style.display =
+    "none";
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
+});
